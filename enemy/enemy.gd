3d9b1f49e58c1d1f691491707849eae07613bde2
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2025-10-29 22:38:49 -0300

    feat: introduction base scene, add script to it, add to group "base" and import

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -2,5 +2,9 @@ extends PathFollow3D
 
 @export var speed: float = 0.1
 
+@onready var base = get_tree().get_first_node_in_group("base")
+
 func _process(delta: float) -> void:
 	progress_ratio += delta * speed
+	if progress_ratio == 1 :
+		base.take_damage()
