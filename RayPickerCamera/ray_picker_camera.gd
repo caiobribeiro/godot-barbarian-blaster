4e3409b4f6ab949cbf6826c592e1d4b8120f2e7d
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2025-11-02 11:51:23 -0300

    feat: enable GridMap cell placement via RayPickerCamera
    Added logic to RayPickerCamera to detect GridMap collisions and set a cell item when the ray collides with an empty cell.

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -1,5 +1,7 @@
 extends Camera3D
 
+@export var gridmap: GridMap
+
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -8,5 +10,13 @@ func _process(delta: float) -> void:
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_postion) * 100.0
 	ray_cast_3d.force_raycast_update()
 	
-	prints(ray_cast_3d.get_collision_point(), ray_cast_3d.get_collider())
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			var collision_point = ray_cast_3d.get_collision_point()
+			var cell = gridmap.local_to_map(collision_point)
+			if gridmap.get_cell_item(cell) == 0:
+				gridmap.set_cell_item(cell, 1)
+
+	
 	
