9141b2f09448f13478773bf5d68594e0599dc2ca
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2025-11-02 22:15:29 -0300

    feat: integrate turret building with ray picker camera
    Added a reference to TurretManager in RayPickerCamera and updated logic to build a turret at the selected grid cell position. Modified base_level.tscn to connect RayPickerCamera to TurretManager.

--- scene/turret_manager.gd
+++ scene/turret_manager.gd
@@ -2,12 +2,13 @@ extends Node3D
 
 @export var turret: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var new_turret = turret.instantiate()
-	add_child(new_turret)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func build_turret(turrent_position: Vector3) -> void: 
+	var new_turret = turret.instantiate()
+	add_child(new_turret)
+	new_turret.global_position = turrent_position
